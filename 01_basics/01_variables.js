4296a459691978f566a1f1ba2b431191a2757b83
akashshakti <av077075@gmail.com>
2026-02-28 14:35:58 +0000

    variables dataType

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "akash@gmmail.com"
+var accountPassword = "123456"
+accountCity = "Ayodhya"
+let accountState
+
+// accountId = 2
+
+accountEmail = "as@gmail.co"
+accountPassword ="88888"
+accountCity = "bengaluru"
+
+console.log(accountId)
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
