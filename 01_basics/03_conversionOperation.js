1a5cd51e5f254bbf65fd18fa61a01d740842aec3
akashshakti <av077075@gmail.com>
2026-03-01 09:59:54 +0000

    condata

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,4 +1,4 @@
-let score = "33aa"
+let score = null
 
 console.log(typeof score);
 console.log(typeof (score));
@@ -6,3 +6,28 @@ console.log(typeof (score));
 let valueInNumber = Number(score)
 console.log(typeof (valueInNumber));
 console.log(valueInNumber);
+
+let ak = true
+
+let valueInNumbe = Number(ak)
+console.log(typeof (valueInNumbe));
+console.log(valueInNumbe);
+
+// "33" => 33
+// "33aa" => NaN
+// true => 1 ; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Akash" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
