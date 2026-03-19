a69ebb9c70888a3dbfd9f193d5b3a7af26b9cfeb
akashshakti <av077075@gmail.com>
2026-03-19 02:51:49 +0000

    string nums and math

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -8,6 +8,7 @@ const repoCount = 10
 // console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
 const gameName = new String('Akash')
+const gameName1 = new String('Akash-av-com')
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
@@ -23,8 +24,21 @@ const gameName = new String('Akash')
 const newString = gameName.substring(0, 3)
 console.log(newString);
 
-const anotherString = gameName.slice(-6, 2)
+const anotherString = gameName.slice(-5, 2)
 console.log(anotherString);
 
 
 // node 01_basics/05_strings.js
+
+const newStringOne = "      Akash      "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://akash.com/akash%20verma"
+
+console.log( url.replace('%20', '-'));
+
+console.log(url.includes('akash'));
+console.log(url.includes('asdjak'));
+
+console.log(gameName1.split('-'));
