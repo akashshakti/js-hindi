1905af0fd18521752010ad3db9827d0574b9c96d
akashshakti <av077075@gmail.com>
2026-03-20 06:57:53 +0000

    date and time

--- 01_basics/07_datesinJs.js
+++ 01_basics/07_datesinJs.js
@@ -13,6 +13,28 @@ let myDate = new Date()
 
 
 // let myCreatedDate = new Date(2026, 1, 19)
-let myCreatedDate = new Date(2026, 1, 19, 5, 3)
-console.log(myCreatedDate.toDateString());
+// let myCreatedDate = new Date(2026, 1, 19, 5, 3)
+// let myCreatedDate = new Date("2026-1-19")
+let myCreatedDate = new Date("02-19-2026")
+// console.log(myCreatedDate.toDateString());
+// console.log(myCreatedDate.toLocaleString());
 
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getDay());
+// console.log(newDate.getMonth() + 1);
+// console.log(newDate.getDate());
+
+// `${newDate.getDay()} and the time `
+
+console.log(newDate.toLocaleString('default', {
+    weekday: "long",
+    timeZone: "IST"
+}));
