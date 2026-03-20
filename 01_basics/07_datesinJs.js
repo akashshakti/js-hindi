663d55a577d0eb440e21f9b0aa4c4b975634d437
akashshakti <av077075@gmail.com>
2026-03-20 05:58:04 +0000

    date

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,16 @@
+// node 01_basics/07_datesinJs.js
+
+// Dates
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toTimeString());
+
+// console.log(typeof myDate);
+
+
+let myCreatedDate = new Date(2026, 1, 19)
+console.log(myCreatedDate.toDateString());
