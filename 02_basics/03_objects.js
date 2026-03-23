723bc17fe59101a235713d8dbf7e0a4d2d76d279
akashshakti <av077075@gmail.com>
2026-03-23 17:17:16 +0000

    7:20

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,21 @@
+// singleton
+// Object.create
+
+
+// object literals
+
+const JsUser = {
+    name: "Akash",
+    "full name" : "Akash ji",
+    age: 18,
+    location: "Ayodhya",
+    email: "akash@akash.com",
+    isLoggeIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+
+}
+
+console.log(JsUser.email);
+console.log(JsUser["email"]);
+console.log(JsUser["email"]);
+
