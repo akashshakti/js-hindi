2c9cd2ef12fc126b4146f973dab70191367c54b2
akashshakti <av077075@gmail.com>
2026-03-24 03:03:49 +0000

    part1 object

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -4,9 +4,13 @@
 
 // object literals
 
+const mySym = Symbol("key1")
+const mySym2 = Symbol("key2")
+
 const JsUser = {
     name: "Akash",
     "full name" : "Akash ji",
+    [mySym]: "mykey1",
     age: 18,
     location: "Ayodhya",
     email: "akash@akash.com",
@@ -15,7 +19,29 @@ const JsUser = {
 
 }
 
-console.log(JsUser.email);
-console.log(JsUser["email"]);
-console.log(JsUser["email"]);
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
+// console.log(JsUser["full name"]);
+// console.log(JsUser[mySym]);
+// console.log(typeof JsUser[mySym]);
 
+JsUser.email = "akash@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "akash@chatti.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+    
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+    
+}
+
+// console.log(JsUser.greeting);
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
+
+
+// ham sab jagha tar . ka use karta hai object ki value ko access kaliya but kuch special cases ma [] ka use karta hai 
