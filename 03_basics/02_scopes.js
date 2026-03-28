5b4cbbff4348acea58bbb1864aae23a9a306fb21
akashshakti <av077075@gmail.com>
2026-03-28 03:11:17 +0000

    scope level & mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -6,17 +6,66 @@ if (true) {
   let a = 10
   const b = 20
   //   var c = 30
-  console.log("INNER: ", a);
+  // console.log("INNER: ", a);
   
 
 } //block scopes
 
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
-    
-}
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+
+function one() {
+  const username = "akash"
+
+  function two() {
+    const website = "youtube"
+    console.log(username);
+    
+  }
+  // console.log(website);
+  
+  two()
+
+}
+
+// one()
+
+if (true) {
+  const username = "akash"
+  if (username === "akash") {
+    const website = " youtube"
+    // console.log(username + website);
+    
+  }
+  // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++++++++++++ interesting ++++++++++++++++++++++++++++++++++
+
+
+function addone(num) {
+  return num + 1
+}
+addone(4)
+
+// +++++++++++++++++
+// const addtwo = function (num) {
+//   return num + 2
+// }
+
+// addtwo(4)
+
+// imp
+addtwo(4)
+
+const addtwo = function (num) {
+  return num + 2
+}
 
