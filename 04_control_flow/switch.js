1ca2358488498ba1f1c9d051bc0f0caf2b55b86d
akashshakti <av077075@gmail.com>
2026-04-07 15:17:25 +0000

    control flow

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,55 @@
+// switch 
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+
+    default:
+        console.log("Default case match");
+        break;
+}
+
+const month1 = "march"
+
+switch (month1) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("Feb");
+        break;
+    case "march":
+        console.log("March");
+        break;
+    case "apr":
+        console.log("April");
+        break;
+    case "may":
+        console.log("May");
+        break;
+    case "june":
+        console.log("June");
+        break;
+
+    default:
+        console.log("Default case match");
+        break;
+}
