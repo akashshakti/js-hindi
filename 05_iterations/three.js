c467ac2ba96c6ae97a07ca58ff399990be2b61c1
akashshakti <av077075@gmail.com>
2026-04-08 12:23:08 +0000

    loop & Array

---
+++ 05_iterations/three.js
@@ -0,0 +1,41 @@
+// for of
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5];
+
+for (const num of arr) {
+  // console.log(num);
+}
+
+const greeting = "Hello World!";
+for (const greet of greeting) {
+  // console.log(greet);
+}
+
+// maps
+
+const map = new Map()
+map.set('IN', "India")
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+
+// console.log(map);
+
+for (const [key, value ] of map) {
+    // console.log(key, ':-', value);   
+}
+
+const myObject = {
+    'game1': "NFS",
+    'game2': "FF",
+    'game3': "PUBG"
+}
+
+// for (const [key, value ] of myObject) {
+//     console.log(key, ':-', value);
+// }
+
+// Ya nahi ho sakta 
