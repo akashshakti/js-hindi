be3532a4aa0f38499c04324de296a94147e6d0b4
Akash <av077075@gmail.com>
2026-05-07 07:08:57 +0000

    oop

---
+++ 10_classes_and_oop/notes.md
@@ -0,0 +1,24 @@
+# javascript and classes
+
+## OOP 
+
+## Object
+- collection of properties and methods 
+- toLowerCase
+
+## why use OOP
+
+## Parts of OOP
+Object literal
+
+- Constructor function
+- Protypes
+- Classes
+- Instances (new, this)
+
+
+## 4 pillars
+Abstraction
+Encapsulation
+Inheritance 
+Polymorphism
---
+++ 10_classes_and_oop/oop.js
@@ -0,0 +1,37 @@
+const user ={
+    username: "Akash",
+    loginCount: 8,
+    signedIn: true,
+
+    getUserDetails: function(){
+        // console.log("Got user details from database");
+        // console.log(`Username: ${this.username}`);
+        // console.log(this);
+        
+    }
+
+}
+
+// console.log(user.username);
+// console.log(user.getUserDetails());
+// console.log(this);
+
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount ;
+    this.isLoggedIn = isLoggedIn ;
+
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`);
+        
+    }
+
+    return this
+}
+
+const userOne = new User("Akash", 12, true)
+const userTwo = new User("Akash", 11, false)
+
+console.log(userOne.constructor);
+// console.log(userTwo);
